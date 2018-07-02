59d63c8c16b8a610144195804ca04e4be568e40a
Arike-work <5284037@qq.com>
2018-07-02 17:44:32 +0800

    logback配置文件完善

--- src/main/java/com/SpringbootApplication.java
+++ src/main/java/com/SpringbootApplication.java
@@ -12,6 +12,5 @@ public class SpringbootApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
-        System.out.println("你说啥");
     }
 }
---
+++ src/main/java/com/bmofang/dao/demo.java
@@ -0,0 +1,18 @@
+package com.bmofang.dao;
+
+/**********************************************
+ *
+ //Copyright© 2014 冷云能源科技有限公司.版权所有
+ *
+ *文件名  ：  springboot.java
+ *文件描述：  class for test
+ *修改日期：  2018-07-02 17:39.
+ *文件作者：  Arike.Y 
+ *
+ **********************************************/
+
+public class demo {
+    public static void run(){
+        System.out.println("这是测试永猎");
+    }
+}
